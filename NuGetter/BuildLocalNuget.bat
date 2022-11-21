4783b936128774bfe0ee67fedfb650cc307db27e
Juha Laukala <juha.laukala@trimble.com>
2022-11-21 13:23:05 +0200

    fixed merge.

--- NuGetter/BuildLocalNuget.bat
+++ NuGetter/BuildLocalNuget.bat
@@ -6,8 +6,4 @@ set /p Build=<ver.txt
 set /a Build=%Build%+1
 echo %Build%>ver.txt
 
-<<<<<<< HEAD
 c:\tools\nuget\nuget.exe pack -Version 0.0.%Build% mimalloc-trimble.nuspec
-=======
-c:\tools\nuget\nuget.exe pack -Version 0.0.%Build% mimalloc.nuspec
->>>>>>> 71cee87bed3ec4dd23aad406300da7f24a11311a
