95e04ec1afd19badd127a26f13af7cc05eb99e2e
Samu Ruuhonen <samu.ruuhonen@trimble.com>
2023-11-08 12:54:29 +0200

    Fixed local nuget build

--- NuGetter/BuildLocalNuget.bat
+++ NuGetter/BuildLocalNuget.bat
@@ -1,9 +1,9 @@
-MSBuild ..\mimalloc.Static.VS2019\mimalloc.Static.VS2019.vcxproj /p:Configuration=Release  /p:Platform=x64
-MSBuild ..\mimalloc.Static.VS2019\mimalloc.Static.VS2019.vcxproj /p:Configuration=DebugCRT /p:Platform=x64
+MSBuild ..\mimalloc.Static.VS2019.sln /p:Configuration=Release  /p:Platform=x64
+MSBuild ..\mimalloc.Static.VS2019.sln /p:Configuration=DebugCRT /p:Platform=x64
 
 @echo off
 set /p Build=<ver.txt
 set /a Build=%Build%+1
 echo %Build%>ver.txt
 
-c:\tools\nuget\nuget.exe pack -Version 0.0.%Build% mimalloc-trimble.nuspec
+c:\work\nuget.exe pack -Version 0.0.%Build%-test mimalloc-trimble.nuspec
